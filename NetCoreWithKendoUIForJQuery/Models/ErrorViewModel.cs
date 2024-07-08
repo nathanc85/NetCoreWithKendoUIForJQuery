5ba8d11e3540c2af2ff7e993d1f93b886fa3dcc7
nathanc85 <cnlnat@yahoo.com>
2024-07-08 17:25:46 -0400

    Initial Commit with testing Kendo UI for jQuery CDN links. It works.

---
+++ NetCoreWithKendoUIForJQuery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetCoreWithKendoUIForJQuery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
